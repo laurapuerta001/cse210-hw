493acb925ad06237568cf0ab443e0835e595ff14
laurapuerta001 <laurapuerta001@gmail.com>
2024-04-27 20:52:01 -0600

    Developer-C# (prep 1-5)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        Console.WriteLine("I'm thinking of a number between 1 and 100.");
+        int guess = -1;
+
+        while (guess != number)
+        {
+
+            Console.Write("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Too high. Guess again.");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Too low. Guess again.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,23 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int answer = -1;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (answer != 0) {
+            Console.Write("Enter a number: ");
+            answer = int.Parse(Console.ReadLine());
+            if (answer != 0) {
+                numbers.Add(answer);
+            }
+        }
+    
+        Console.WriteLine("The total is: " + numbers.Sum());
+        Console.WriteLine("The average is: " + (numbers.Sum() / numbers.Count).ToString());
+        Console.WriteLine("The largest number is: " + numbers.Max());
+
     }
 }
