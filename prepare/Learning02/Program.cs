e8ca21577924625dc6e77f884abb940dd6dc7919
laurapuerta001 <laurapuerta001@gmail.com>
2024-05-02 09:40:33 -0600

    Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job = new Job();
+        job._company = "Microsoft";
+        job._startYear = 1975;
+        job._endYear = 1980;
+        job._jobTitle = "Software Engineer";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume resume = new Resume();
+        resume._name = "Luciana Puerta";
+        
+        resume._jobs.Add(job);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
