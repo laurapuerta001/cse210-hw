b7fd4720a20a11a13d8e715aa73cbf5eac93b17a
laurapuerta001 <laurapuerta001@gmail.com>
2024-05-27 14:53:27 -0600

    Lerning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red", 4);
+
+        Rectangle rectangle = new Rectangle("blue", 4, 5);
+
+        Circle circle = new Circle("green", 4);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine($"Color: {shape.GetColor()}");
+        }
     }
 }
